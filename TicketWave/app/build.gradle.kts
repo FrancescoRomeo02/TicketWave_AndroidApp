e29b403dde86edd1e29e4941aba795d436966f5b
Mattia <mattiatrombella02@gmail.com>
2023-10-20 11:48:16 +0200

    update target version

--- TicketWave/app/build.gradle.kts
+++ TicketWave/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "it.marcosoft.ticketwave"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "it.marcosoft.ticketwave"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -28,12 +28,14 @@ android {
     }
 }
 
-dependencies {
 
+dependencies {
+    implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
