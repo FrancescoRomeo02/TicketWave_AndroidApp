d089156faad88fb52f765ce35086f7f65d1efe75
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2023-12-12 21:35:38 +0100

    calendar page from Soranno

--- TicketWave/app/src/main/java/it/marcosoft/ticketwave/util/LikedFragment.java
+++ TicketWave/app/src/main/java/it/marcosoft/ticketwave/util/LikedFragment.java
@@ -23,7 +23,7 @@ import it.marcosoft.ticketwave.R;
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_liked, container, false);
+            return inflater.inflate(R.layout.calendar, container, false);
         }
     }
 
