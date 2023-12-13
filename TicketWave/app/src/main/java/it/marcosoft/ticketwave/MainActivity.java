e415eed3c11e38531c9fca3f261f6217872e98e4
Ppopi <filipponembrini200@gmail.com>
2023-12-13 19:17:53 +0100

    Revert "Merge branch 'Passoni' into Nembrini"
    This reverts commit 968f153ec1543ccec581d9dac82da3e6e61b7df7.

--- TicketWave/app/src/main/java/it/marcosoft/ticketwave/MainActivity.java
+++ TicketWave/app/src/main/java/it/marcosoft/ticketwave/MainActivity.java
@@ -2,48 +2,97 @@ package it.marcosoft.ticketwave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.text.Html;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.fragment.app.Fragment;
+
+
+import it.marcosoft.ticketwave.util.DiscoverFragment;
+import it.marcosoft.ticketwave.util.LikedFragment;
+import it.marcosoft.ticketwave.util.TravelFragment;
+import it.marcosoft.ticketwave.util.SharedPreferencesUtil;
+import it.marcosoft.ticketwave.util.UserAuthenticationUtil;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-        if(user == null){
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        }
-        else {
-            textView.setText(user.getEmail());
-        }
+        // Check and create preferences file if not exists
+        SharedPreferencesUtil.checkAndCreatePreferencesFile(this);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        // Check the login status
+        if (UserAuthenticationUtil.getLoginStatus(this) | true) {
+            // User is logged in, launch the main activity
+            setContentView(R.layout.activity_main);
+
+            bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+            // Load the default fragment
+            loadFragment(new DiscoverFragment());
+            //bottomNavigationView.getMenu().findItem(R.id.discover).setTitle("Discover");
+            //bottomNavigationView.getMenu().findItem(R.id.liked).setTitle(null);
+            //bottomNavigationView.getMenu().findItem(R.id.calendar).setTitle(null);
+
+            bottomNavigationView.setSelectedItemId(R.id.discover);
+
+
+            // Set listener for BottomNavigationView item clicks
+            bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+                Fragment fragment;
+
+                int itemId = item.getItemId();
+
+
+                if (itemId == R.id.liked) {
+                    fragment = new LikedFragment();
+                    item.setTitle("Liked");
+                    //bottomNavigationView.getMenu().findItem(R.id.discover).setTitle(null);
+                    //bottomNavigationView.getMenu().findItem(R.id.calendar).setTitle(null);
+                   
+                } else if (itemId == R.id.discover) {
+                    item.setTitle("Discover");
+                    //bottomNavigationView.getMenu().findItem(R.id.liked).setTitle(null);
+                    //bottomNavigationView.getMenu().findItem(R.id.calendar).setTitle(null);
+                    // Usa DiscoverFragment come schermata principale
+                    fragment = new DiscoverFragment();
+                } else if (itemId == R.id.calendar) {
+                    fragment = new TravelFragment();
+                    item.setTitle("Add Travel");
+                    //bottomNavigationView.getMenu().findItem(R.id.discover).setTitle(null);
+                    //bottomNavigationView.getMenu().findItem(R.id.liked).setTitle(null);
+                } else {
+                    return false;
+                }
+
+
+                return loadFragment(fragment);
+            });
+        } else {
+            // User is not logged in, launch the login activity
+            setContentView(R.layout.activity_register);
+            // Add additional logic for the login activity if needed
+        }
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
