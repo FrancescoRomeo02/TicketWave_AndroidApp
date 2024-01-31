1ec8c44de9a84f6b0fefb22686f8fb56bf1b35d9
Mattia <mattiatrombella02@gmail.com>
2024-01-31 15:36:51 +0100

    recyclerview and dynamic cards

--- TicketWave/app/src/main/java/it/marcosoft/ticketwave/util/DiscoverFragment.java
+++ TicketWave/app/src/main/java/it/marcosoft/ticketwave/util/DiscoverFragment.java
@@ -6,18 +6,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import it.marcosoft.ticketwave.CardAdapter;
+import it.marcosoft.ticketwave.CardData;
+import it.marcosoft.ticketwave.MainActivity;
 import it.marcosoft.ticketwave.R;
 
-
 public class DiscoverFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        CardData[] cardData = new CardData[]{
+                new CardData("Madrid", "26 gennaio", "13 febbraio"),
+                new CardData("Madrid", "26 gennaio", "13 febbraio"),
+                new CardData("Madrid", "26 gennaio", "13 febbraio"),
+        };
+
+        CardAdapter cardAdapter = new CardAdapter(cardData, (MainActivity) getActivity());
+        recyclerView.setAdapter(cardAdapter);
+
+        return rootView;
     }
 }
-
