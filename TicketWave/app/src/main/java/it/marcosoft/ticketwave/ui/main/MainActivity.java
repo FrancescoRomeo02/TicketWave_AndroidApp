635eddc95dc3185d6706fbcbd0752c4fab092af6
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2024-02-05 15:17:55 +0100

    5 feb pome: 93% del progetto finito (i colori sono belli come Cassano)

--- TicketWave/app/src/main/java/it/marcosoft/ticketwave/ui/main/MainActivity.java
+++ TicketWave/app/src/main/java/it/marcosoft/ticketwave/ui/main/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Check and create preferences file if not exists
         SharedPreferencesUtil.checkAndCreatePreferencesFile(this);
+        //SharedPreferencesUtil.setLoginStatus(this, true);
 
 
 
@@ -32,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Check the login status
         if ( SharedPreferencesUtil.getLoginStatus(this)) {
 
-            //TODO rimuovi i due comandi qua sotto, servono finche' non c'e' un bottone di logout
-            Log.d("S","DebugMSGxLogin, ricorda di rimuovere");
-            //SharedPreferencesUtil.setLoginStatus(this,false);
+            SharedPreferencesUtil.setLoginStatus(this,false);
 
             // User is logged in, launch the main activity
             setContentView(R.layout.activity_main);
