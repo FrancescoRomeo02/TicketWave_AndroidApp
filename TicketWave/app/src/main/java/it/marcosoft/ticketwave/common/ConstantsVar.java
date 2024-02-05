a3354e6d1e5fb7c375f4aa94e38009666e9ab815
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2024-02-05 19:51:58 +0100

    5 feb tardo-pome: 99.5%

--- TicketWave/app/src/main/java/it/marcosoft/ticketwave/common/ConstantsVar.java
+++
@@ -1,8 +0,0 @@
-package it.marcosoft.ticketwave.common;
-
-
-public class ConstantsVar {
-    public static final String DISCOVERY_EVENTS_ENDPOINT = "discovery/v2/events";
-    public static String USER_NAME = "";
-    // Add more constants for other endpoints if needed
-}
